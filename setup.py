b8092773965cbfc10e8cd00f7102b9c29188200c
bacps-iscteiulpt <bacps@iscte-iul.pt>
2018-08-22 16:43:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     keywords='Firebase',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-         'requests>=2.11.0',
+        'requests>=2.11.0',
         'google-cloud-storage>=1.8.0',
         'google-auth>=1.4.0',
         'requests-toolbelt>=0.7.0'
